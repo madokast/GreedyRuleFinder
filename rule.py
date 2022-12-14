e875f14ae45dadda8bcfa219322cbdf6c7c3de46
赵润晓 <zhaorunxiao@sics.ac.cn>
2022-12-14 11:04:06 +0800

    impl 'or' operate in rule

--- rule.py
+++ rule.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 import pandas as pd
 import sqlite3
 import os 
@@ -16,6 +16,7 @@ class Predicate:
         self.t1_col = t1_col
         self.operator = operator
         self.constant = constant
+        self._columns = set([t0_col]) if t1_col is None else set([t0_col, t1_col])
     
     @staticmethod
     def newConst0(col:str, constant:str, operator:str = '=')->'Predicate':
@@ -33,6 +34,23 @@ class Predicate:
     
     def isConst(self)->bool:
         return self.constant is not None
+
+    def columns(self)->Set[str]:
+        return self._columns
+    
+    def copy(self)->'Predicate':
+        return copy.deepcopy(self)
+    
+    def negate(self)->'Predicate':
+        def negateOp(op:str)->str:
+            if op == '=':
+                return '<>'
+            else:
+                raise ValueError('NoImpl ' + op)
+
+        p = self.copy()
+        p.operator = negateOp(p.operator)
+        return p
     
     def __str__(self) -> str:
         return self.__repr__()
@@ -50,49 +68,72 @@ class Predicate:
         return self.__repr__()
 
 class Rule:
-    def __init__(self, Xs:List[Predicate] = [], y:Predicate = None, rowSize:int = 0, xSupp:int = 0, supp:int = 0) -> None:
-        self.Xs = Xs
+    def __init__(self, andXs:List[Predicate] = [], orXs:List[Predicate] = [], y:Predicate = None, sameTable:bool = True, rowSize:int = 0, xSupp:int = 0, supp:int = 0) -> None:
+        self.andXs = andXs
+        self.orXs = orXs
         self.y = y
+        self.sameTable = sameTable
         self.rowSize = rowSize
         self.xSupp = xSupp
         self.supp = supp
 
+    def copy(self)->'Rule':
+        return copy.deepcopy(self)
+
     def singleLine(self)->bool:
-        for x in self.Xs:
+        for x in self.andXs:
+            if not x.isConst():
+                return False
+        for x in self.orXs:
             if not x.isConst():
                 return False
         
         return self.y.isConst()
 
-    def rowSizeSQL(self, sameTable:bool = True)->str:
+    def rowSizeSQL(self)->str:
         if self.singleLine():
             return f"SELECT count(*) FROM {SQL_TAB0} AS t0"
         else:
             sql = f"SELECT count(*) FROM {SQL_TAB0} AS t0, {SQL_TAB1} AS t1"
-            if sameTable:
+            if self.sameTable:
                 sql += f" WHERE t0.{SQL_ID_COL} <> t1.{SQL_ID_COL}"
             return sql
     
-    def xSuppSQL(self, sameTable:bool = True)->str:
-        sql = f"SELECT count(*) FROM {SQL_TAB0} AS t0 "
-        wheres = [p.sql() for p in self.Xs]
-        if not self.singleLine():
-            sql += f", {SQL_TAB1} AS t1 "
-            if sameTable:
-                wheres.append(f"t0.{SQL_ID_COL} <> t1.{SQL_ID_COL}")
-        return sql + " WHERE " + " AND ".join(wheres)
+    def xSuppSQL(self)->str:
+        sql = f"SELECT count(*) FROM {SQL_TAB0} AS t0"
+        andWheres = [p.sql() for p in self.andXs]
+        orWheres = [p.sql() for p in self.orXs]
 
-    def suppSQL(self, sameTable:bool = True)->str:
-        return self.xSuppSQL(sameTable) + " AND " + self.y.sql()
+        if not self.singleLine():
+            sql += f", {SQL_TAB1} AS t1"
+            if self.sameTable:
+                andWheres.append(f"t0.{SQL_ID_COL} <> t1.{SQL_ID_COL}")
+
+        assert len(andWheres) > 0 
+        andWhere = " AND ".join(andWheres)
+        orWhere = " OR ".join(orWheres)
+        if len(orWheres) > 0:
+            orWhere = " OR ".join(orWheres)
+            andWhere += f" AND ({orWhere})" 
+        return sql + " WHERE " + andWhere
+
+    def suppSQL(self)->str:
+        return self.xSuppSQL() + " AND " + self.y.sql()
     
     def __str__(self) -> str:
         return self.__repr__()
 
     def __repr__(self) -> str:
-        x = " ".join((str(p) for p in self.Xs))
+        andX = " ^ ".join((str(p) for p in self.andXs))
+        orX = " | ".join((str(p) for p in self.orXs))
+        if len(self.orXs) > 0:
+            orX = "(" + orX + ")"
         cover = round(self.supp/self.rowSize, 2)
         conf = round(self.supp/self.xSupp, 2)
-        return f"{x} -> {self.y}, rowSize={self.rowSize}, xSupp={self.xSupp}, supp={self.supp}, covre={cover}, conf={conf}"
+
+        if len(orX) > 0:
+            orX += " ^ " 
+        return f"{orX}{andX} -> {self.y}, rowSize={self.rowSize}, xSupp={self.xSupp}, supp={self.supp}, covre={cover}, conf={conf}"
 
 def _rm(file:str):
     if os.path.exists(file):
@@ -116,9 +157,9 @@ def ruleCheck(rules:List[Rule], t0:pd.DataFrame, t1:pd.DataFrame = None):
         t0.to_sql(SQL_TAB0, conn)
         t1.to_sql(SQL_TAB1, conn)
         for rule in rules:
-            rule.rowSize = conn.execute(rule.rowSizeSQL(sameTable)).fetchone()[0]
-            rule.xSupp = conn.execute(rule.xSuppSQL(sameTable)).fetchone()[0]
-            rule.supp = conn.execute(rule.suppSQL(sameTable)).fetchone()[0]
+            rule.rowSize = conn.execute(rule.rowSizeSQL()).fetchone()[0]
+            rule.xSupp = conn.execute(rule.xSuppSQL()).fetchone()[0]
+            rule.supp = conn.execute(rule.suppSQL()).fetchone()[0]
     finally:
         conn.close()
         _rm(SQLITE3_TEMP_FILE)
@@ -130,9 +171,11 @@ def ruleCheck(rules:List[Rule], t0:pd.DataFrame, t1:pd.DataFrame = None):
 if __name__ == '__main__':
     data = pd.read_csv("testdata/relation.csv", dtype=str)
     print(data)
-
-    rule1 = Rule([Predicate.newConst0("ac", "908")], Predicate.newConst0("cc", "01"))
-    rule2 = Rule([Predicate.newStruct("cc")], Predicate.newStruct("ac"))
+    rule1 = Rule(andXs = [Predicate.newConst0("pn", "2222222"), Predicate.newConst0("ac", "908", "<>"), Predicate.newConst0("ct", "EDI", "<>")], 
+        y = Predicate.newConst0("cc", "01"))
+    rule2 = Rule(andXs = [Predicate.newStruct("cc")], y = Predicate.newStruct("ac"))
+    print(rule1.suppSQL())
+    print(rule2.suppSQL())
     ruleCheck([rule1, rule2], data)
     print(rule1)
     print(rule2)
